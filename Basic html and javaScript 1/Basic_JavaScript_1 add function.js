9f38d8f4feb6d6695b4e519dd84fb6c80094727e
damein2612 <dameinwhite@yahoo.com>
2022-06-14 15:09:24 -0700

    added files

---
+++ Basic html and javaScript 1/Basic_JavaScript_1 add function.js
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<h2>JavaScript Alert</h2>
+
+<button onclick="myFunction()">Try it</button>
+
+<script>
+	function My_First_Function() {
+		var String = "Kiss me, I'm Irsih!";
+		var result = String.fontcolor ("green");
+		document.getElementById ("Irish").innerHTML =
+		result;
+}
+function myFunction() {
+  alert("I am an alert box!");
+}
+</script>
+
+</body>
+</html>
